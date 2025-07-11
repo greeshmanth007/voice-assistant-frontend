e80169632bcd3a686fbaf3ef2d48968b26a320fa
greeshmanth007 <lettertogreeshmanth@gmail.com>
2025-07-11 17:16:23 +0530

    button issue resolved

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function toggleListening() {
     button.style.backgroundColor = '#facc15';
   }
 }
+
+
+function toggleListening2() {
+  const button = document.getElementById('speakBtn-2');
+  const span = document.getElementById('speakText-2');
+  if (span.textContent === 'Speak Now') {
+    span.textContent = 'I am Listening...';
+    button.style.backgroundColor = '#ef4444';
+  } else {
+    span.textContent = 'Speak Now';
+    button.style.backgroundColor = '#facc15';
+  }
+}
